06383106a1d2eca977373f85a0f49618fd2dd877
Hs-the-dev <odioneghogho@gmail.com>
2025-01-06 06:07:41 +0100

    test 2

--- src/components/product/ProductDescription.tsx
+++ src/components/product/ProductDescription.tsx
@@ -1,13 +1,3 @@
-// import React from "react";
-
-// export default function ProductDescription({ product }) {
-//   return (
-//     <div className="w-[50%]">
-//       <h1>{product.title}</h1>
-//       <p>{product.description}</p>
-//     </div>
-//   );
-// }
 import React from "react";
 
 const ProductDescription = () => {

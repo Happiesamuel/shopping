aa51b01ad7c7600885f4fb6381d8c4778cf72c36
Hs-the-dev <odioneghogho@gmail.com>
2025-01-06 10:48:25 +0100

    test 11

--- src/app/categories/[category]/page.tsx
+++ src/app/categories/[category]/page.tsx
@@ -1,13 +1,17 @@
 import List from "@/components/List";
 import React, { Suspense } from "react";
 
-const page = ({ params }: { params: { category: string } }) => {
-  const id = params.category;
+type PageProps = {
+  params: { category: string };
+};
+
+const page = ({ params }: PageProps) => {
+  const { category } = params;
   return (
     <div>
       <h1>Category</h1>
       <Suspense fallback={<p>loading...</p>}>
-        <List id={id} />
+        <List id={category} />
       </Suspense>
     </div>
   );
--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+type PageProps = {
+  params: { productId: string };
+};
 
-const page = ({ params }: { params: { productId: string } }) => {
-  const id = params.productId;
-  console.log(id);
+const page = ({ params }: PageProps) => {
+  const { productId } = params;
+  console.log(productId);
   return <div>page</div>;
 };
 
